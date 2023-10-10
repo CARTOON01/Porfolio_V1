f5d07aad3af84a05306ef2870e47adee553fbcfa
CARTOON01 <brianngugi224@gmail.com>
2023-10-10 14:53:22 +0300

    header

---
+++ src/components/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let y;
+
+    let tabs = [
+        {name: "Projects", link: "#projects" },
+        {name: "About me", link: "#about" },
+    ]
+</script>
+
+<header>
+    <h1>
+        <b>Brian</b> Ngugi
+    </h1>
+    <div class="">
+        {#each tabs as tab, index}
+        <a>
+            <p>{tab.name}</p>
+        </a>
+            
+        {/each}
+    </div>
+</header>
